d2a610fd328a9dea08174af7f35d768214b87a19
A1eXFei <alexfeifan@gmail.com>
2018-11-12 12:01:55 +0800

    commit
    Move train and predict to parent class

--- model/base_network.py
+++ model/base_network.py
@@ -2,7 +2,9 @@ import tensorflow as tf
 import configparser
 from util import log_util
 from util import fn_util
+from util import dl_util
 import os
+import datetime
 
 
 # TODO: eager execution
@@ -23,6 +25,7 @@ class Network:
 
     def __init_hyper_param(self):
         self.model_name = self.config.get("Model", "name")
+        self.model_type = self.config.get("Model", "type")
         self.batch_size = self.config.getint("Dataset", "batch_size")
         self.repeat = self.config.getint("Dataset", "repeat_time")
 
@@ -43,35 +46,149 @@ class Network:
     def _init_network(self):
         pass
 
-    def _var_summaries(self, var):
-        with tf.name_scope("Summaries"):
-            mean = tf.reduce_mean(var)
-            tf.summary.scalar("mean", mean)
-            with tf.name_scope("stddev"):
-                stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-            tf.summary.scalar("stddev", stddev)
-            tf.summary.scalar("max", tf.reduce_max(var))
-            tf.summary.scalar("min", tf.reduce_min(var))
-            tf.summary.histogram("histogram", var)
+    # def _var_summaries(self, var):
+    #     with tf.name_scope("Summaries"):
+    #         mean = tf.reduce_mean(var)
+    #         tf.summary.scalar("mean", mean)
+    #         with tf.name_scope("stddev"):
+    #             stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    #         tf.summary.scalar("stddev", stddev)
+    #         tf.summary.scalar("max", tf.reduce_max(var))
+    #         tf.summary.scalar("min", tf.reduce_min(var))
+    #         tf.summary.histogram("histogram", var)
 
     def _add_train_ops(self):
         with tf.name_scope("Loss"):
             with tf.name_scope(self.loss_fn):
                 self.cost = fn_util.get_loss_fn(self.loss_fn, self.y_, self.network)
                 tf.summary.scalar(self.loss_fn, self.cost)
+
         with tf.name_scope("Train_Step"):
             optimizer = fn_util.get_opt_fn(self.opt_fn)
             self.train_step = optimizer(self.learning_rate).minimize(self.cost)
         with tf.name_scope("Accuracy"):
+
             self.accuracy = fn_util.get_acc_fn(self.acc_fn, self.y_, self.network)
 
-    def train(self):
-        pass
+    def train(self, dataset):
+        if self.model_type == "RNN":
+            dataset = dataset.apply(tf.contrib.data.batch_and_drop_remainder(self.time_steps * self.batch_size))
+            # dataset = dataset.batch(self.batch_size * self.time_steps, drop_remainder= True) # available in tf 1.10
+        else:
+            dataset = dataset.batch(self.batch_size)
 
-    def eval(self):
-        pass
+        dataset = dataset.repeat(self.repeat)
+        iterator = dataset.make_one_shot_iterator()
 
-    def predict(self):
+        # 2018/08/06 - To fully utilize CPUs for training
+        # config = tf.ConfigProto(device_count={"CPU": 8},
+        #                         inter_op_parallelism_threads=16,
+        #                         intra_op_parallelism_threads=16,
+        #                         log_device_placement=True
+        #                         )
+        #
+        # with tf.Session(config=config) as sess:
+        with tf.Session() as sess:
+            min_cost = 100
+            best_accuracy = 0
+            avg_accuracy = 0
+            merged = tf.summary.merge_all()
+            saver = tf.train.Saver(max_to_keep=5)
+
+            if self.tensorboard_summary_enabled:
+                writer = tf.summary.FileWriter(self.log_dir, sess.graph)
+
+            next_xs, next_ys = iterator.get_next()
+
+            init = tf.global_variables_initializer()
+            sess.run(init)
+
+            current_step = 0
+            while current_step < self.echo:
+                raw_xs, raw_ys = sess.run([next_xs, next_ys])
+
+                batch_xs = dl_util.dict_to_list(raw_xs)
+
+                if self.type == "classification":
+                    batch_ys = dl_util.one_hot(raw_ys)
+                else:
+                    batch_ys = raw_ys
+
+                if self.model_type == "RNN":
+                    batch_xs = batch_xs.reshape((-1, self.time_steps, self.input_size))
+                    batch_ys = dl_util.rnn_output_split(batch_ys, self.time_steps, self.output_size)
+
+                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                run_metadata = tf.RunMetadata()
+                summary, _, cost, accuracy = sess.run([merged, self.train_step, self.cost, self.accuracy],
+                                                      feed_dict={self.x: batch_xs, self.y_: batch_ys},
+                                                      options=run_options,
+                                                      run_metadata=run_metadata)
+
+                if self.tensorboard_summary_enabled:
+                    writer.add_summary(summary, current_step)
+
+                if min_cost > cost:
+                    saver.save(sess, self.model_dir + self.model_name, global_step=current_step)
+                    min_cost = cost
+
+                if best_accuracy <= accuracy:
+                    best_accuracy = accuracy
+
+                if current_step > 10000:
+                    avg_accuracy = avg_accuracy + accuracy
+
+                if current_step % 100 == 0:
+                    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                    print("Step " + str(current_step) + ": cost is " + str(cost))
+                    self.logger.info("Step " + str(current_step) + ": cost is " + str(cost))
+                    _, acc = sess.run([merged, self.accuracy], feed_dict={self.x: batch_xs, self.y_: batch_ys})
+                    print("Accuracy is: " + str(acc))
+                    self.logger.info("Accuracy is: " + str(acc))
+
+                current_step = current_step + 1
+
+            if self.tensorboard_summary_enabled:
+                writer.close()
+
+            print("----------------------------------------------------------")
+            print("Best Accuracy is: " + str(best_accuracy))
+            self.logger.info("Best Accuracy is: " + str(best_accuracy))
+            print("Average Accuracy is: " + str(round(avg_accuracy / (self.echo - 10000), 2)))
+            self.logger.info("Average Accuracy is: " + str(round(avg_accuracy / (self.echo - 10000), 2)))
+
+    def eval(self, dataset):
+        if not os.path.exists(self.model_dir):
+            raise ModelNotTrained()
+
+        if len(os.listdir(self.model_dir)) > 0:
+            print("----------------------------------------------------------")
+
+            dataset = dataset.batch(self.batch_size)
+            dataset = dataset.repeat(self.repeat)
+            iterator = dataset.make_one_shot_iterator()
+
+            with tf.Session() as sess:
+                next_xs, next_ys = iterator.get_next()
+
+                init = tf.global_variables_initializer()
+                sess.run(init)
+
+                saver = tf.train.Saver()
+                # saver = tf.train.import_meta_graph(tf.train.latest_checkpoint(self.model_dir)+".meta")
+                # print(tf.train.latest_checkpoint(self.model_dir))
+                saver.restore(sess, tf.train.latest_checkpoint(self.model_dir))
+
+                raw_xs, raw_ys = sess.run([next_xs, next_ys])
+                batch_xs = dl_util.dict_to_list(raw_xs)
+                batch_ys = dl_util.one_hot(raw_ys)
+
+                acc = sess.run(self.accuracy, feed_dict={self.x: batch_xs, self.y_: batch_ys})
+                print("Accuracy for evaluation is: " + str(acc))
+        else:
+            raise ModelNotTrained()
+
+    def predict(self, batch_x):
         pass
 
 
--- model/rnn.py
+++ model/rnn.py
@@ -1,5 +1,3 @@
-import datetime
-from util import dl_util
 from model.base_network import *
 
 MNIST_BOUNDARIES = [0, 1, 2, 3, 4, 5, 6, 7, 8]
@@ -66,82 +64,6 @@ class RNN(Network):
             act = fn_util.get_act_fn(self.config.get("Output", "act_fn"))
             self.network = tf.contrib.layers.fully_connected(self.network[-1], self.output_size, activation_fn=act)
             print("Building Output Layer:Output Size =>" + str(self.output_size))
-
-    def train(self, dataset):
-        dataset = dataset.apply(tf.contrib.data.batch_and_drop_remainder(self.time_steps * self.batch_size))
-        # dataset = dataset.batch(self.batch_size * self.time_steps, drop_remainder= True) # available in tf 1.10
-        dataset = dataset.repeat(self.repeat)
-        iterator = dataset.make_one_shot_iterator()
-
-        # 2018-08-06 - To fully utilize CPUs for training
-        config = tf.ConfigProto(device_count={"CPU": 8},
-                                inter_op_parallelism_threads=16,
-                                intra_op_parallelism_threads=16,
-                                log_device_placement=True
-                                )
-
-        with tf.Session(config=config) as sess:
-            min_cost = 100
-            best_accuracy = 0
-            avg_accuracy = 0
-            merged = tf.summary.merge_all()
-            saver = tf.train.Saver(max_to_keep=5)
-
-            if self.tensorboard_summary_enabled:
-                writer = tf.summary.FileWriter(self.log_dir, sess.graph)
-
-            next_xs, next_ys = iterator.get_next()
-
-            init = tf.global_variables_initializer()
-            sess.run(init)
-
-            global_step = 0
-            while global_step < self.echo:
-                raw_xs, raw_ys = sess.run([next_xs, next_ys])
-                # batch_xs = raw_xs # No need to use dict_to_list because MNIST is ndarry
-                batch_xs = dl_util.dict_to_list(raw_xs)
-                batch_xs = batch_xs.reshape((-1, self.time_steps, self.input_size))
-                # batch_ys = dl_util.one_hot(raw_ys, boundaries=MNIST_BOUNDARIES)
-                batch_ys = dl_util.one_hot(raw_ys)
-                batch_ys = dl_util.rnn_output_split(batch_ys, self.time_steps, self.output_size)
-
-                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-                run_metadata = tf.RunMetadata()
-                summary, _, cost, accuracy = sess.run([merged, self.train_step, self.cost, self.accuracy],
-                                                      feed_dict={self.x: batch_xs, self.y_: batch_ys},
-                                                      options=run_options,
-                                                      run_metadata=run_metadata)
-
-                if self.tensorboard_summary_enabled:
-                    writer.add_summary(summary, global_step)
-
-                if min_cost > cost:
-                    saver.save(sess, self.model_dir + self.model_name, global_step=global_step)
-                    min_cost = cost
-
-                if best_accuracy <= accuracy:
-                    best_accuracy = accuracy
-
-                if global_step > 10000:
-                    avg_accuracy = avg_accuracy + accuracy
-
-                if global_step % 100 == 0:
-                    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-                    print("Step " + str(global_step) + ": cost is " + str(cost))
-                    self.logger.info("Step " + str(global_step) + ": cost is " + str(cost))
-                    _, acc = sess.run([merged, self.accuracy], feed_dict={self.x: batch_xs, self.y_: batch_ys})
-                    print("Accuracy is: " + str(acc))
-                    self.logger.info("Accuracy is: " + str(acc))
-                global_step = global_step + 1
-
-            if self.tensorboard_summary_enabled:
-                writer.close()
-
-            print("----------------------------------------------------------")
-            print("Best Accuracy is: " + str(best_accuracy))
-            self.logger.info("Best Accuracy is: " + str(best_accuracy))
-            print("Average Accuracy is: " + str(round(avg_accuracy / (self.echo - 10000), 2)))
-            self.logger.info("Average Accuracy is: " + str(round(avg_accuracy / (self.echo - 10000), 2)))
 
     def eval(self, dataset):
         pass
