f425d0ff3cb38f7fce484574037c28d2b4c06ff3
A1eXFei <alexfeifan@gmail.com>
2019-01-26 18:20:20 +0800

    commit

--- rl_main.py
+++ rl_main.py
@@ -3,17 +3,29 @@ from rl.agent.base import *
 # from rl.agent.dqn_agent import DQNAgent
 from rl.agent.dqn_agent_c import DQNAgent, DQNConfig
 from rl.env.cerebro_ext import RLExtCerebro
+from os import listdir
+from os.path import join
 import backtrader as bt
 import tensorflow as tf
+import random
 
 episode = 50
-data_path = "./test_data/stock/000002.csv"
-scaled_data_path = ""
-
+data_dir = "D:\\output\\"
+# data_path = "./test_data/stock/000002.csv"
+# scaled_data_path = ""
 network_config_path = "./config/stock_mlp_baseline.cls"
 
 
+def get_data_files(file_list):
+    index = random.randint(0, len(file_list) - 1)
+    sub_path = join(data_dir, file_list[index])
+    csv_files = [join(sub_path, f) for f in listdir(sub_path) if f != ".DS_Store"]
+    return csv_files
+
+
 if __name__ == "__main__":
+    data_files = listdir(data_dir)
+
     with tf.Session() as sess:
         # agent = DQNAgent(network_config_path, sess)
         config = DQNConfig()
@@ -22,6 +34,9 @@ if __name__ == "__main__":
 
         for i in range(episode):
             print("#####################EPISODE " + str(i) + "###########################")
+
+            data_path = get_data_files(data_files)[0]
+            print("Current file is: " + data_path)
 
             cerebro = RLExtCerebro()
 
